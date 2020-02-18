e9aefb1e04b33121ff3a33d5649ca9626b04f8ed
Andrew Curzon <andrew.w.curzon@gmail.com>
2020-02-18 14:07:46 -0600

    init

---
+++ lemonadeStand/Game.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lemonadeStand
+{
+    class Game
+    {
+        string captureInput;
+
+        public Game()
+        {
+
+        }
+
+        void InputDecision()
+        {
+            while (true)
+            {
+                UserInterface.DrawStarterMenu();
+                captureInput = Console.ReadLine();
+                while (captureInput != "1" && captureInput != "2" && captureInput != "3")
+                {
+                    UserInterface.InputError();
+                }
+                if (captureInput == "1")
+                {
+                    PlayGame();
+                }
+                else if (captureInput == "2")
+                {
+                    UserInterface.Rules();
+                }
+                else if (captureInput == "3")
+                {
+                    Environment.Exit(0);
+                }
+            }
+        }
+
+        void PlayGame()
+        {
+
+        }
+
+        public void Initialize()
+        {
+            UserInterface.GameIntroduction();
+            UserInterface.DrawStarterMenu();
+            InputDecision();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ lemonadeStand/UserInterface.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lemonadeStand
+{
+    static class UserInterface
+    {
+        static string welcomeMessage = "Welcome to Lemonade Stand!";
+        static string starterMenu = "What would you like to do?\n" +
+            "1.) Play\n" +
+            "2.) See rules\n" +
+            "3.) Exit";
+        static string inputError = "Incorrect input. Try again.";
+        static string rules = "The rules";
+        static string pressEnterToContinue = "\nPress [ENTER] to continue.";
+
+        static public void GameIntroduction()
+        {
+            Console.WriteLine(welcomeMessage);
+            System.Threading.Thread.Sleep(1500);
+        }
+
+        static public void DrawStarterMenu()
+        {
+            Console.Clear();
+            Console.WriteLine(starterMenu);
+        }
+
+        static public void InputError()
+        {
+            Console.Clear();
+            Console.WriteLine(inputError);
+        }
+
+        static public void Rules()
+        {
+            Console.Clear();
+            Console.WriteLine(rules);
+            Console.WriteLine(pressEnterToContinue);
+            Console.ReadLine();
+        }
+    }
+}
